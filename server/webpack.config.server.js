691c5d9d8d1e6499dcceaa405ed8d69cb7fe9fe7
Ray Alez <raymestalez@gmail.com>
2017-04-17 15:26:45 -0700

    Update.

--- server/webpack.config.server.js
+++ server/webpack.config.server.js
@@ -4,10 +4,10 @@ var ExternalsPlugin = require('webpack-externals-plugin');
 
 module.exports = {
 
-  entry: path.resolve(__dirname, 'server.js'),
+  entry: path.resolve(__dirname, './server/server.js'),
 
   output: {
-      path: __dirname + '/dist/',
+      path: __dirname + './server/dist/',
       filename: 'server.bundle.js',
   },
 
