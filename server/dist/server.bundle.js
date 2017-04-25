2bb3f2fbae8d3a97e06bebe354e18c9474f19f64
Ray Alez <raymestalez@gmail.com>
2017-04-25 01:36:34 -0700

    Google Analytics.

--- server/dist/server.bundle.js
+++ server/dist/server.bundle.js
@@ -492,7 +492,11 @@ var Post = function (_Component) {
 	_createClass(Post, [{
 		key: 'componentWillMount',
 		value: function componentWillMount() {
-			this.props.fetchSettings();
+			/* Fetch settings if there aren't any.
+      Not sure if this does anything, maybe Im just fetching them from header.*/
+			if (!this.props.settings.metaTitle) {
+				this.props.fetchSettings();
+			}
 		}
 	}, {
 		key: 'renderPostHeader',
@@ -1019,7 +1023,8 @@ var Editor = function (_Component) {
 						placeholder: "Write here... (can use markdown)",
 						initialValue: this.props.postForm.body,
 						indentWithTabs: false,
-						tabSize: 4
+						tabSize: 4,
+						autoDownloadFontAwesome: false
 					} }),
 				_react2.default.createElement(_reactBootstrap.FormControl, { className: "post-tags",
 					type: 'text',
