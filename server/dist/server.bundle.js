ecc59f5194a0793bf514f86f33618a1b1fc8f89c
Ray Alez <raymestalez@gmail.com>
2017-05-19 00:31:09 -0700

    Update.

--- server/dist/server.bundle.js
+++ server/dist/server.bundle.js
@@ -1306,7 +1306,7 @@ exports.API_URL = API_URL;
 var settings = {
     title: "lumen<b>writes</b>",
     domain: "https://lumenwrites.com",
-    categories: ["fiction", "jokes", "meta"],
+    categories: ["fiction", "jokes"],
     username: "lumen",
     about: "I make dumb jokes. Sometimes I write stories. SciFi is fun. <br/> <br/>  If you like my stuff - go read a <a href='https://nulis.io/blog/'>blog</a> where I share everything I've learned in the process of trying to get good at this(start <a href='https://nulis.io/blog/post/nulis-writing-prompts-e58092f'>here</a>). <br/><br/> Wanna talk? Email to lumenwrites@gmail.com.",
     userurl: "https://lumenwrites.com",
